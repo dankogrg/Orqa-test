57df1c1ba50f7cf5e0eaa59c9b05e889f6140ad5
dankogrg <dankogrg@gmail.com>
2024-12-06 17:48:15 +0100

    manual chart diagram finished

--- src/Services/chartObject.js
+++ src/Services/chartObject.js
@@ -31,8 +31,8 @@ export const getGraphData = async () => {
         }
     }
 
-    if (tree.subordinates) {
-        for (const i of tree.subordinates) {
+    const extendTree = (branch) => {
+        for (const i of branch) {
             for (const ii of employeeList) {
                 if (i != ii) {
                     if (i.id == ii.manager_id) {
@@ -45,6 +45,202 @@ export const getGraphData = async () => {
                 }
             }
         }
+    };
+    extendTree(tree.subordinates);
+
+    for (const i of tree.subordinates) {
+        for (const ii of i.subordinates) {
+            for (const iii of employeeList) {
+                if (ii != iii) {
+                    if (ii.id == iii.manager_id) {
+                        if (!ii.subordinates) {
+                            ii.subordinates = [];
+                        }
+
+                        ii.subordinates.push(iii);
+                    }
+                }
+            }
+        }
     }
+
+    for (const i of tree.subordinates) {
+        for (const ii of i.subordinates) {
+            if (ii.subordinates) {
+                for (const iii of ii.subordinates) {
+                    for (const iv of employeeList) {
+                        if (iii != iv) {
+                            if (iii.id == iv.manager_id) {
+                                if (!iii.subordinates) {
+                                    iii.subordinates = [];
+                                }
+
+                                iii.subordinates.push(iv);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    for (const i of tree.subordinates) {
+        for (const ii of i.subordinates) {
+            if (ii.subordinates) {
+                for (const iii of ii.subordinates) {
+                    if (iii.subordinates) {
+                        for (const iv of iii.subordinates) {
+                            for (const index of employeeList) {
+                                if (iv != index) {
+                                    if (iv.id == index.manager_id) {
+                                        if (!iv.subordinates) {
+                                            iv.subordinates = [];
+                                        }
+
+                                        iv.subordinates.push(index);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    for (const i of tree.subordinates) {
+        for (const ii of i.subordinates) {
+            if (ii.subordinates) {
+                for (const iii of ii.subordinates) {
+                    if (iii.subordinates) {
+                        for (const iv of iii.subordinates) {
+                            if (iv.subordinates) {
+                                for (const v of iv.subordinates) {
+                                    for (const index of employeeList) {
+                                        if (v != index) {
+                                            if (v.id == index.manager_id) {
+                                                if (!v.subordinates) {
+                                                    v.subordinates = [];
+                                                }
+
+                                                v.subordinates.push(index);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (const i of tree.subordinates) {
+        for (const ii of i.subordinates) {
+            if (ii.subordinates) {
+                for (const iii of ii.subordinates) {
+                    if (iii.subordinates) {
+                        for (const iv of iii.subordinates) {
+                            if (iv.subordinates) {
+                                for (const v of iv.subordinates) {
+                                    if (v.subordinates) {
+                                        for (const vi of v.subordinates) {
+                                            for (const index of employeeList) {
+                                                if (vi != index) {
+                                                    if (vi.id == index.manager_id) {
+                                                        if (!vi.subordinates) {
+                                                            vi.subordinates = [];
+                                                        }
+
+                                                        vi.subordinates.push(index);
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (const i of tree.subordinates) {
+        for (const ii of i.subordinates) {
+            if (ii.subordinates) {
+                for (const iii of ii.subordinates) {
+                    if (iii.subordinates) {
+                        for (const iv of iii.subordinates) {
+                            if (iv.subordinates) {
+                                for (const v of iv.subordinates) {
+                                    if (v.subordinates) {
+                                        for (const vi of v.subordinates) {
+                                            if (vi.subordinates) {
+                                                for (const vii of vi.subordinates) {
+                                                    for (const index of employeeList) {
+                                                        if (vii != index) {
+                                                            if (vii.id == index.manager_id) {
+                                                                if (!vii.subordinates) {
+                                                                    vii.subordinates = [];
+                                                                }
+
+                                                                vii.subordinates.push(index);
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (const i of tree.subordinates) {
+        for (const ii of i.subordinates) {
+            if (ii.subordinates) {
+                for (const iii of ii.subordinates) {
+                    if (iii.subordinates) {
+                        for (const iv of iii.subordinates) {
+                            if (iv.subordinates) {
+                                for (const v of iv.subordinates) {
+                                    if (v.subordinates) {
+                                        for (const vi of v.subordinates) {
+                                            if (vi.subordinates) {
+                                                for (const vii of vi.subordinates) {
+                                                    if (vii.subordinates) {
+                                                        for (const viii of vii.subordinates) {
+                                                            for (const index of employeeList) {
+                                                                if (viii != index) {
+                                                                    if (viii.id == index.manager_id) {
+                                                                        if (!viii.subordinates) {
+                                                                            viii.subordinates = [];
+                                                                        }
+
+                                                                        viii.subordinates.push(index);
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     console.log(tree);
 };
