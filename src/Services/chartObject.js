49f510ac7968ceec2b84e15f97f06ff3a24235be
dankogrg <dankogrg@gmail.com>
2024-12-10 12:32:00 +0100

    done chart object and doing chart display

--- src/Services/chartObject.js
+++ src/Services/chartObject.js
@@ -3,7 +3,7 @@ import { getEmployees } from '../utils/fetch';
 export const getAllEmployees = async () => {
     let i = 1;
     let graphData = [];
-    console.log(graphData);
+
     let result;
     do {
         result = await getEmployees(i);
@@ -18,230 +18,36 @@ export const getAllEmployees = async () => {
 
 export const getGraphData = async () => {
     const employeeList = await getAllEmployees();
-    console.log(employeeList);
-    const tree = employeeList[0];
-    for (const i of employeeList) {
-        if (i != tree) {
-            if (tree.id == i.manager_id) {
-                if (!tree.subordinates) {
-                    tree.subordinates = [];
-                }
-                tree.subordinates.push(i);
-            }
-        }
+
+    let map = {};
+    let rootArray = [];
+    for (let entry of employeeList) {
+        map[entry.id] = { expanded: false, data: entry };
     }
 
-    const extendTree = (branch) => {
-        for (const i of branch) {
-            for (const ii of employeeList) {
-                if (i != ii) {
-                    if (i.id == ii.manager_id) {
-                        if (!i.subordinates) {
-                            i.subordinates = [];
-                        }
-
-                        i.subordinates.push(ii);
-                    }
-                }
-            }
-        }
-    };
-    extendTree(tree.subordinates);
-
-    for (const i of tree.subordinates) {
-        for (const ii of i.subordinates) {
-            for (const iii of employeeList) {
-                if (ii != iii) {
-                    if (ii.id == iii.manager_id) {
-                        if (!ii.subordinates) {
-                            ii.subordinates = [];
-                        }
-
-                        ii.subordinates.push(iii);
-                    }
-                }
-            }
-        }
+    for (let entry of employeeList) {
+        entry.subordinates = [];
     }
 
-    for (const i of tree.subordinates) {
-        for (const ii of i.subordinates) {
-            if (ii.subordinates) {
-                for (const iii of ii.subordinates) {
-                    for (const iv of employeeList) {
-                        if (iii != iv) {
-                            if (iii.id == iv.manager_id) {
-                                if (!iii.subordinates) {
-                                    iii.subordinates = [];
-                                }
+    console.log(map);
 
-                                iii.subordinates.push(iv);
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    let root = { expanded: false, data: {} };
+    root.data = employeeList.find((x) => x.manager_id == null);
+
+    if (root == null) return null;
+
+    for (let entry of employeeList) {
+        if (entry === root.data) continue;
+        let parent = map[entry.manager_id];
+
+        parent.data.subordinates.push(map[entry.id]);
     }
-    for (const i of tree.subordinates) {
-        for (const ii of i.subordinates) {
-            if (ii.subordinates) {
-                for (const iii of ii.subordinates) {
-                    if (iii.subordinates) {
-                        for (const iv of iii.subordinates) {
-                            for (const index of employeeList) {
-                                if (iv != index) {
-                                    if (iv.id == index.manager_id) {
-                                        if (!iv.subordinates) {
-                                            iv.subordinates = [];
-                                        }
-
-                                        iv.subordinates.push(index);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    root.expanded = true;
+    for (const entry of root.data.subordinates) {
+        entry.expanded = true;
     }
-    for (const i of tree.subordinates) {
-        for (const ii of i.subordinates) {
-            if (ii.subordinates) {
-                for (const iii of ii.subordinates) {
-                    if (iii.subordinates) {
-                        for (const iv of iii.subordinates) {
-                            if (iv.subordinates) {
-                                for (const v of iv.subordinates) {
-                                    for (const index of employeeList) {
-                                        if (v != index) {
-                                            if (v.id == index.manager_id) {
-                                                if (!v.subordinates) {
-                                                    v.subordinates = [];
-                                                }
+    rootArray.push(root);
+    console.log(rootArray);
 
-                                                v.subordinates.push(index);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    for (const i of tree.subordinates) {
-        for (const ii of i.subordinates) {
-            if (ii.subordinates) {
-                for (const iii of ii.subordinates) {
-                    if (iii.subordinates) {
-                        for (const iv of iii.subordinates) {
-                            if (iv.subordinates) {
-                                for (const v of iv.subordinates) {
-                                    if (v.subordinates) {
-                                        for (const vi of v.subordinates) {
-                                            for (const index of employeeList) {
-                                                if (vi != index) {
-                                                    if (vi.id == index.manager_id) {
-                                                        if (!vi.subordinates) {
-                                                            vi.subordinates = [];
-                                                        }
-
-                                                        vi.subordinates.push(index);
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    for (const i of tree.subordinates) {
-        for (const ii of i.subordinates) {
-            if (ii.subordinates) {
-                for (const iii of ii.subordinates) {
-                    if (iii.subordinates) {
-                        for (const iv of iii.subordinates) {
-                            if (iv.subordinates) {
-                                for (const v of iv.subordinates) {
-                                    if (v.subordinates) {
-                                        for (const vi of v.subordinates) {
-                                            if (vi.subordinates) {
-                                                for (const vii of vi.subordinates) {
-                                                    for (const index of employeeList) {
-                                                        if (vii != index) {
-                                                            if (vii.id == index.manager_id) {
-                                                                if (!vii.subordinates) {
-                                                                    vii.subordinates = [];
-                                                                }
-
-                                                                vii.subordinates.push(index);
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    for (const i of tree.subordinates) {
-        for (const ii of i.subordinates) {
-            if (ii.subordinates) {
-                for (const iii of ii.subordinates) {
-                    if (iii.subordinates) {
-                        for (const iv of iii.subordinates) {
-                            if (iv.subordinates) {
-                                for (const v of iv.subordinates) {
-                                    if (v.subordinates) {
-                                        for (const vi of v.subordinates) {
-                                            if (vi.subordinates) {
-                                                for (const vii of vi.subordinates) {
-                                                    if (vii.subordinates) {
-                                                        for (const viii of vii.subordinates) {
-                                                            for (const index of employeeList) {
-                                                                if (viii != index) {
-                                                                    if (viii.id == index.manager_id) {
-                                                                        if (!viii.subordinates) {
-                                                                            viii.subordinates = [];
-                                                                        }
-
-                                                                        viii.subordinates.push(index);
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    console.log(tree);
-    return tree;
+    return rootArray;
 };
--- src/views/Chart.jsx
+++ src/views/Chart.jsx
@@ -1,6 +1,50 @@
-import { getAllEmployees, getGraphData } from '../Services/chartObject';
+import { useEffect, useState } from 'react';
+import { getGraphData } from '../Services/chartObject';
+import { OrganizationChart } from 'primereact/organizationchart';
 
-export default function Chart() {
-    getGraphData();
-    return <div>Chart</div>;
-}
+const Chart = () => {
+    const [selection, setSelection] = useState([]);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        try {
+            getGraphData()
+                .then((result) => setData(result))
+                .catch((error) => console.warn(error));
+        } catch (error) {
+            console.log(error);
+        }
+    }, []);
+
+    console.log(data);
+
+    const nodeTemplate = (node) => {
+        return (
+            <div className="flex flex-column">
+                <div className="flex flex-column align-items-center">
+                    <img
+                        alt={`${node.data.firstName} ${node.data.lastName}`}
+                        src={node.data.imageUrl}
+                        className="mb-3 w-3rem h-3rem"
+                    />
+                    <span className="font-bold mb-2">{`${node.firstName} ${node.lastName}`}</span>
+                    <span>{node.data.position}</span>
+                </div>
+            </div>
+        );
+    };
+
+    return (
+        <div className="card overflow-x-auto">
+            <OrganizationChart
+                value={data}
+                selectionMode="multiple"
+                selection={selection}
+                onSelectionChange={(e) => setSelection(e.data)}
+                nodeTemplate={nodeTemplate}
+            />
+        </div>
+    );
+};
+
+export default Chart;
